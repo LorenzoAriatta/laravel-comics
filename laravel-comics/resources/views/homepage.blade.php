8d37e5d20cbff7c081f6ba6a76c3d1fe59445d8d
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-05-18 15:42:09 +0200

    heade e init main

--- laravel-comics/resources/views/homepage.blade.php
+++ laravel-comics/resources/views/homepage.blade.php
@@ -5,24 +5,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/app.css">
     <title>DC-Comics</title>
 </head>
 
 <body>
 
-    <header>
-        @yield('header');
-        @yield('jumbotron');
-    </header>
 
-    <main>
-        @yield('spotlight');
-        @yield('shop-links');
-    </main>
+    @include('partials/header')
+
+
+
+    @include('partials/main')
+    @yield('spotlight')
+    @yield('shop-links')
+
 
     <footer>
-        @yield('top-footer');
-        @yield('bot-footer');
+        @yield('top-footer')
+        @yield('bot-footer')
     </footer>
 
 </body>
---
+++ laravel-comics/resources/views/partials/main.blade.php
@@ -0,0 +1,10 @@
+<main>
+    <div class="jumbo"></div>
+    <div class="main-container">
+        <div class="flydiv">
+            <h2>CURRENT SERIES</h2>
+        </div>
+
+    </div>
+    <button>LOAD MORE</button>
+</main>
