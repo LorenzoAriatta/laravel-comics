d0532b0744f17cd149215be1e4d70fe1735a3031
Lorenzo Ariatta <lorenzotk77@gmail.com>
2022-05-18 19:23:41 +0200

    completato

--- laravel-comics/resources/views/partials/detail.blade.php
+++ laravel-comics/resources/views/partials/detail.blade.php
@@ -1,17 +1,36 @@
 <main>
     <div class="jumbo"><img src="images/jumbotron.jpg" alt="jumbo"></div>
-    <div class="main-container">
+    <div class="blue-band"></div>
+    <div class="detail-container">
 
-        <article>
-
-            <div class="cards">
-                <a href="#"> <img src={{ $comics[0]['thumb'] }} alt="coverComics" /></a>
-                <p>{{ $comics[0]['title'] }}</p>
+        <div class="flyimg">
+            <a href="#"> <img src={{ $comics[0]['thumb'] }} alt="coverComics" /></a>
+        </div>
+        <div class="small-container">
+            <div class="detail-content">
+                <h1>{{ $comics[0]['title'] }}</h1>
+                <div class="detail-price">
+                    <div class="price">
+                        <div class="usPrice">
+                            <h5>U.S. Price: <span>{{ $comics[0]['price'] }}</span></h5>
+                        </div>
+                        <div class="avalaible">
+                            <h5>AVAILABLE</h5>
+                        </div>
+                    </div>
+                    <div class="check">
+                        <h5>Check Availability</h5>
+                    </div>
+                </div>
+                <p>{{ $comics[0]['description'] }}</p>
             </div>
+            <div class="advertisement">
+                <h5>ADVERTISEMENT</h5>
+                <img src="images/adv.jpg" alt="">
+            </div>
+        </div>
 
-            <button><a href="/">GO BACK</a></button>
-
-        </article>
 
     </div>
+    <button><a href="/">GO BACK</a></button>
 </main>
